79ebf3262171542c3b5d923c9d92c5431b5880cc
bibarub <bibarub@proton.me>
2025-03-01 16:06:30 +0300

    readd a few missing control sequences

---
+++ text/spcomp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+
+if len(sys.argv) != 2:
+    print('usage: spcomp.py scene.txt')
+    sys.exit()
+
+with open(sys.argv[1], encoding='UTF-8') as f:
+    lines = f.readlines()
+
+for i in range(3):
+    lines.pop(0)
+
+for i in range(len(lines) // 3):
+    i *= 3
+    if lines[i+2] == '\n': continue
+    count_jp = lines[i+1].count('%')
+    count_en = lines[i+2].count('%')
+    if count_jp != count_en:
+        print('line {}: mismatch! {} in jp, {} in en'.format(i+3+1, count_jp, count_en))
+        print(lines[i+1], lines[i+2])
