49f4070ab5af8305fa0949a17550a1d7d1792736
bibarub <bibarub@proton.me>
2025-03-09 16:18:41 +0300

    bye-bye, single quotes

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,7 @@
 rm -rf e17_mac/ e17_etc/ e17_iso_extracted/ e17_mac_en/ e17_etc_en/ e17_se/ e17_se_mod/ e17_bg/ e17_bg_en/
 rm -rf bin/
 rm -rf workdir
-rm -rf text/mac-en-only
+rm -rf text/mac-en-only*/
 rm -rf text/tmp
 rm -f text/font/FONT00.FNT text/font/FONT00.NEW
 rm -rf text/font/glyphs
